0ad6aeb89d56f3962c33b888350cae28d307f45d
Raul Sanz <luar_znas@hotmail.com>
2021-03-10 00:23:52 +0000

    StartButton click funtion is fixed; Questions, answers and the solution are arrays

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,97 +21,60 @@ var wordBlank = document.querySelector(".wordblank");
 var timerCount = document.querySelector(".timer-text");
 var win = document.querySelector(".win");
 var lose = document.querySelector(".lose");
+var app = document.querySelector(".app");
 
 
 // Game starts
-startButton.addEventListener("click", startTimer);
+startButton.addEventListener("click", function() {
+    app.innerHTML = "";
+    
+    // Timer
+    var timerCount = 75;
+    var timeIntervalUp = setInterval(function() { startTimer() }, 1000);
 
+    function startTimer() {
+        document.getElementById("countDown").innerHTML = timerCount;
+        timerCount--;
 
-// Timer
-var timerCount = 75;
-var timeIntervalUp = setInterval(function() { startTimer() }, 1000);
-
-function startTimer() {
-    document.getElementById("countDown").innerHTML = timerCount;
-    timerCount--;
-
-    if (timerCount == 0) {
-        clearInterval(timeIntervalUp);
-        endTimer();
-        reload();
+        if (timerCount == 0) {
+            clearInterval(timeIntervalUp);
+            endTimer();
+            reload();
+        }
     }
-}
+});
+
 
 function endTimer() {
     document.getElementById("countDown").innerHTML = "The game is over";
 }
 
-// HTML structure
-
-var h2El = document.createElement("h2");
-var listEl = document.createElement("ol");
-var li1 = document.createElement("li");
-var li2 = document.createElement("li");
-var li3 = document.createElement("li");
-var li4 = document.createElement("li");
-
-// Add text for list items
-h2El.textContent = question1.question;
-li1.textContent = question1.answerA;
-li2.textContent = question1.answerB;
-li3.textContent = question1.answerC;
-li4.textContent = question1.answerD;
-
-// Append questions items
-body.appendChild(h2El);
-listEl.appendChild(li1);
-listEl.appendChild(li2);
-listEl.appendChild(li3);
-listEl.appendChild(li4);
-
 
 // QUESTIONS & ANSWERS
-var question1 = {
-    question: "Commonly used data types DO NOT include:",
-    answerA: "A. Strings",
-    answerB: "B. Booleans",
-    answerC: "C. Alerts",
-    answerD: "D. Numbers",
-    correctAnswer: "c",
-}
-
-var question2 = {
-    question: "The condition in an if/else statement is enclosed within _______.",
-    answerA: "A. Quotes",
-    answerB: "B. Curly brackets",
-    answerC: "C. Parenthesis",
-    answerD: "D. Square brackets",
-    correctAnswer: "c",
-}
-
-var question3 = {
-    question: "Arrays in JavaScript can be used to store _______.",
-    answerA: "A. Numbers and strings",
-    answerB: "B. Other arrays",
-    answerC: "C. Booleans",
-    answerD: "D. All of the above",
-    correctAnswer: "d",
-}
-
-var question4 = {
-    question: "String values must be enclosed within _______ when being assigned to variables.",
-    answerA: "A. Commas",
-    answerB: "B. Curly brackets",
-    answerC: "C. Quotes",
-    answerD: "D. Parenthesis",
-    correctAnswer: "d",
-}
-
-var question5 = {
-    question: "A very useful tool used during development and debugging for printing contect to the debugger is:",
-    answerA: "A. JavaScript",
-    answerB: "B. Terminal / Bash",
-    answerC: "C. For loops",
-    answerD: "D. Console.log",
-    correctAnswer: "d",
-}
+var questions = [
+    {
+        question: "Commonly used data types DO NOT include:",
+        answers: ["A. Strings", "B. Booleans", "C. Alerts", "D. Numbers"],
+        correctAnswer: 2
+    },
+    {
+        question: "The condition in an if/else statement is enclosed within _______.",
+        answers: ["A. Quotes", "B. Curly brackets", "C. Parenthesis", "D. Square brackets"],
+        correctAnswer: 2
+    },
+    {
+        question: "Arrays in JavaScript can be used to store _______.",
+        answers: ["A. Numbers and strings", "B. Other arrays", "C. Booleans", "D. All of the above"],
+        correctAnswer: 3
+    },
+    {
+        question: "String values must be enclosed within _______ when being assigned to variables.",
+        answers: ["A. Commas", "B. Curly brackets", "C. Quotes", "D. Parenthesis"],
+        correctAnswer: 3
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing contect to the debugger is:",
+        answers: ["A. JavaScript", "B. Terminal / Bash", "C. For loops", "D. Console.log"],
+        correctAnswer: 3
+    }
+];
