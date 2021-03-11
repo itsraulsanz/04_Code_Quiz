e7dc097f4ea30ffdabcc9c6d9e3e19d83f4f5570
Raul Sanz <luar_znas@hotmail.com>
2021-03-11 18:43:59 +0000

    Fixing timer; Local Storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,114 +14,148 @@
                 - I clear the highscores
                     - All the scores saved are deleted
 */
-
+var questions = [
+  {
+    question: "Commonly used data types DO NOT include:",
+    answers: ["A. Strings", "B. Booleans", "C. Alerts", "D. Numbers"],
+    solution: 2,
+  },
+  {
+    question:
+      "The condition in an if/else statement is enclosed within _______.",
+    answers: [
+      "A. Quotes",
+      "B. Curly brackets",
+      "C. Parenthesis",
+      "D. Square brackets",
+    ],
+    solution: 2,
+  },
+  {
+    question: "Arrays in JavaScript can be used to store _______.",
+    answers: [
+      "A. Numbers and strings",
+      "B. Other arrays",
+      "C. Booleans",
+      "D. All of the above",
+    ],
+    solution: 3,
+  },
+  {
+    question:
+      "String values must be enclosed within _______ when being assigned to variables.",
+    answers: ["A. Commas", "B. Curly brackets", "C. Quotes", "D. Parenthesis"],
+    solution: 3,
+  },
+  {
+    question:
+      "A very useful tool used during development and debugging for printing contect to the debugger is:",
+    answers: [
+      "A. JavaScript",
+      "B. Terminal / Bash",
+      "C. For loops",
+      "D. Console.log",
+    ],
+    solution: 3,
+  },
+];
 var startButton = document.querySelector(".startQuiz");
 var timerCount = document.querySelector(".timer-text");
 var app = document.getElementById("gameStart");
 var app = document.getElementById("app");
-
+var currentQuestionIndex = 0;
 document.getElementById("app").style.visibility = "hidden";
-
-
-
+var score = 0; 
+var timerCount = 75;
+var timeIntervalUp;
 // Game starts
-startButton.addEventListener("click", function() {
-    gameStart.innerHTML = "";
-    document.getElementById("app").style.visibility = "visible";
-    
-    // Timer
-    var timerCount = 75;
-    var timeIntervalUp = setInterval(function() { startTimer() }, 1000);
-
-    function startTimer() {
-        document.getElementById("countDown").innerHTML = timerCount;
-        timerCount--;
-
-        if (timerCount == 0) {
-            clearInterval(timeIntervalUp);
-            endTimer();
-            reload();
-        }
-    }
-    
-    function endTimer() {
-    document.getElementById("countDown").innerHTML = "The game is over";
-
-}
+startButton.addEventListener("click", function () {
+  gameStart.innerHTML = "";
+  document.getElementById("app").style.visibility = "visible";
+  // Timer
+  timeIntervalUp = setInterval(startTimer, 1000);
+  showQuestion();
 });
 
+function startTimer() {
+  document.getElementById("countDown").innerHTML = timerCount;
+  timerCount--;
+
+  if (timerCount == 0) {
+    clearInterval(timeIntervalUp);
+    endTimer();
+  }
+}
+
+function endTimer() {
+  document.getElementById("countDown").innerHTML = "The game is over";
+}
 
 // QUESTIONS & ANSWERS
-var questions = [
-    {
-        question: "Commonly used data types DO NOT include:",
-        answers: ["A. Strings", "B. Booleans", "C. Alerts", "D. Numbers"],
-        solution: 2
-    },
-    {
-        question: "The condition in an if/else statement is enclosed within _______.",
-        answers: ["A. Quotes", "B. Curly brackets", "C. Parenthesis", "D. Square brackets"],
-        solution: 2
-    },
-    {
-        question: "Arrays in JavaScript can be used to store _______.",
-        answers: ["A. Numbers and strings", "B. Other arrays", "C. Booleans", "D. All of the above"],
-        solution: 3
-    },
-    {
-        question: "String values must be enclosed within _______ when being assigned to variables.",
-        answers: ["A. Commas", "B. Curly brackets", "C. Quotes", "D. Parenthesis"],
-        solution: 3
-    },
-    {
-        question: "A very useful tool used during development and debugging for printing contect to the debugger is:",
-        answers: ["A. JavaScript", "B. Terminal / Bash", "C. For loops", "D. Console.log"],
-        solution: 3
-    }
-];
 
+function showQuestion() {
+  var step = questions[currentQuestionIndex];
+  var questionSection = document.createElement("section");
+  var title = document.createElement("h2");
+  var answers = document.createElement("ul");
+  var result = document.createElement("p");
+  app.innerHTML = "";
 
-function showQuestion(stepIndex) {
-    var step = questions[stepIndex];
-    var questionSection = document.createElement("section");
-    var title = document.createElement("h2");
-    var answers = document.createElement("ul");
-    var result = document.createElement("p");
-    app.innerHTML = "";
-  
-    function answerClick(i) {
-      var isCorrectAnswer = i === step.solution;
-  
-      if (isCorrectAnswer) {
-        //alert("CORRECT");
-        result.textContent = "Correct answer!";
-      } else {
-        //alert("WRONG");
-        result.textContent = "Wrong answer.";
-        timeIntervalUp = timeIntervalUp - 10;
-      }
-  
-      showQuestion(stepIndex + 1);
-    }
-  
-    for (let i = 0; i < step.answers.length; i++) {
-      var li = document.createElement("li");
-      li.textContent = step.answers[i];
-  
-      li.addEventListener("click", function () {
-        answerClick(i);
-      });
-  
-      answers.appendChild(li);
-    }
-  
-    title.innerHTML = step.question;
-  
-    questionSection.appendChild(title);
-    questionSection.appendChild(answers);
-    questionSection.appendChild(result);
-  
-    document.getElementById("app").appendChild(questionSection);
+  for (let i = 0; i < questions[currentQuestionIndex].answers.length; i++) {
+    var li = document.createElement("li");
+    li.textContent = questions[currentQuestionIndex].answers[i];
+
+    li.addEventListener("click", function () {
+      answerClick(i);
+    });
+
+    answers.appendChild(li);
   }
-  
-  showQuestion(0);
+
+  title.innerHTML = questions[currentQuestionIndex].question;
+
+  questionSection.appendChild(title);
+  questionSection.appendChild(answers);
+  questionSection.appendChild(result);
+
+  document.getElementById("app").appendChild(questionSection);
+}
+function answerClick(i) {
+  var isCorrectAnswer = i === questions[currentQuestionIndex].solution;
+  console.log(isCorrectAnswer);
+  if (isCorrectAnswer) {
+    alert("CORRECT");
+    score++;
+    //result.textContent = "Correct answer!";
+    //console.log(result);
+  } else {
+    alert("WRONG");
+    // result.textContent = "Wrong answer.";
+    timerCount -= 10;
+    document.getElementById("countDown").innerHTML = timerCount;
+  }
+  currentQuestionIndex++;
+  if (currentQuestionIndex === questions.length) {
+    endQuiz();
+  } else {
+    showQuestion();
+  }
+}
+
+function endQuiz() {
+    // Clear the screen and present back an input box with the players score
+    //When they enter their initials they can click a button which will get the value from the box 
+var playersScore = {
+    score: score,
+    initials: "RS"
+}
+
+    // LOCAL STORAGE
+
+    // create a variable which is assigned whatever is returned from get ... from local storage OR ELSE and empty array
+    var highscores = JSON.parse(window.localStorage.getItem("highscores")) || []; 
+
+    highscores.push(playersScore);
+
+    window.localStorage.setItem("highscores", JSON.stringify(highscores))
+}
